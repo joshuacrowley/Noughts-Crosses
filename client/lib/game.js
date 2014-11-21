3c033a95c26807c557b73f3225372fd285882b78
Joshua Crowley <joshua.crowley@gmail.com>
2014-11-22 10:37:18 +1100

    improved structure

--- client/lib/game.js
+++ client/lib/game.js
@@ -1,14 +1,10 @@
-Boxes = new Mongo.Collection("boxes");
-Games = new Mongo.Collection("games");
-
-
-
-function checkBoxes (){
+checkBoxes = function (){
     var usersGame = Meteor.user().profile.currentGame;
     var toCheck = Boxes.find({"gameToken": usersGame}, { $sort : { boxOrder : -1}}).fetch();
     var toGame = Games.find({"gameToken": usersGame}).fetch();
-    console.log(toCheck[1]);
-    console.log(toGame);
+    
+    //console.log(toCheck[1]);
+    //console.log(toGame);
 
     var value = (Meteor.userId() == toGame[0]["owner"] ) ? "nought" : "cross";
     console.log(value);
@@ -29,11 +25,10 @@ function checkBoxes (){
     };
 }
 
-function startGame () {
+startGame = function (){
 
   var token = Random.id([8]);
-
-  console.log(token);
+  //console.log(token);
   Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.currentGame": token}});
 
     Games.insert({
@@ -51,30 +46,5 @@ function startGame () {
         createdAt: new Date()
       });
     };
+    
 };
-
-Template.menu.events({
-    "click .box": function (event, template) {
-    var usersGame = Meteor.user().profile.currentGame; 
-    var toGame = Games.find({"gameToken": usersGame}).fetch(); 
-    var value = (Meteor.userId() == toGame[0]["owner"] ) ? "nought" : "cross";
-    Boxes.update(this._id, {$set: {boxValue: value}});
-    checkBoxes(value);
-  },
-
-  "click #start": function (event, template) {    
-    console.log("start");
-    startGame();
-    var usersGame = Meteor.user().profile.currentGame;
-    Router.go('games', {_id: usersGame});
-  },
-
-  "click #reset": function (event, template) {
-    Meteor.call('resetBoxes');
-  },
-
-  "click #join": function (event, template) {
-    Router.go('post.show');
-  }
-
-  });
--- server/server.js
+++ server/server.js
@@ -1,5 +1,2 @@
-Boxes = new Mongo.Collection("boxes");
-Games = new Mongo.Collection("games");
-
 process.env.MAIL_URL="smtp://joshua.crowley%40gmail.com:goodbyemorning@smtp.gmail.com:465/";
 
